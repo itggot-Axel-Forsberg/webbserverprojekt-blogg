7857be7da00b015e98c97fadf9b80ad132f1dd8d
itggot-Axel-Forsberg <35598696+itggot-Axel-Forsberg@users.noreply.github.com>
2019-02-24 16:31:19 +0100

    Fixade register och Login funktion
    Gjorde klart min login funktion och min register funktion på min bloggsida.

---
+++ app.rb
@@ -0,0 +1,49 @@
+require 'byebug'
+require 'sinatra'
+require 'slim'
+require 'sqlite3'
+require 'bcrypt'
+
+get('/') do
+    slim(:index)
+end
+
+get('/register') do
+    slim(:register)
+end
+
+post('/create') do
+    db = SQLite3::Database.new('db/Users.db')
+    db.results_as_hash = true
+
+    hashat_password = BCrypt::Password.create(params["Password"])
+
+    db.execute("INSERT INTO users (Username, Email, Password) VALUES(?, ?, ?)", params["Username"], params["Email"], hashat_password)
+
+    redirect('/login')
+end
+
+get('/login') do
+    slim(:login)
+end
+
+post('/login') do
+    db = SQLite3::Database.new('db/Users.db')
+    db.results_as_hash = true
+    pass = db.execute("SELECT Password FROM users WHERE Username = ?",params["Username"])
+    
+    if (BCrypt::Password.new(pass.first["Password"]) == params["Password"]) == true
+        redirect('/mypage')
+    else
+        redirect('/error')
+    end
+    
+end
+
+get('/mypage') do
+    slim(:index)
+end
+
+get('/error') do
+    slim(:error)
+end
